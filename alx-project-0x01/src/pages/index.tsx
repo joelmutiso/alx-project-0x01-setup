9b6fdba79ae1b21becfdc174ad4e1d0974c52963
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-26 19:05:18 +0300

    Initial Next.js project setup

---
+++ alx-project-0x01/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-7xl font-thin">Welcome Page</h1>
+
+    </div>
+  )
+}
+
+export default Home;
